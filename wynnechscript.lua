5d51120cff04283789e09c7db4bcb3aa9850c502
wynnech4202 <133446348+wynnech4202@users.noreply.github.com>
2023-06-24 19:45:55 -0500

    Update wynnechscript.lua

--- wynnechscript.lua
+++ wynnechscript.lua
@@ -558,18 +558,18 @@ end
 Page.Button({
     Text = "improved instant kill",
     Callback = function()
-   task.spawn(function()
+  task.spawn(function()
     while wait() do
         game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):WaitForChild("SwordSwingMiss"):FireServer({
             ["weapon"] = game:GetService("ReplicatedStorage").Items.diamond_great_hammer,
             ["chargeRatio"] = 1
         })
         game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):WaitForChild("SwordSwingMiss"):FireServer({
-            ["weapon"] = game:GetService("ReplicatedStorage").Items.diamond_great_hammer_2,
+            ["weapon"] = game:GetService("ReplicatedStorage").Items.diamond_great_hammer,
             ["chargeRatio"] = 1
         })
         game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):WaitForChild("SwordSwingMiss"):FireServer({
-            ["weapon"] = game:GetService("ReplicatedStorage").Items.diamond_great_hammer_3,
+            ["weapon"] = game:GetService("ReplicatedStorage").Items.diamond_great_hammer,
             ["chargeRatio"] = 1
         })
     end
